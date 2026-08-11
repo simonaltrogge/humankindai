912b5ac2338e267156d75ee1b8f5745d8c43fb31
Simon Altrogge <8720147+simonaltrogge@users.noreply.github.com>
2026-08-11 18:36:57 +0200

    ✨ Add efficacies of humankind and ai

--- src/humainkind/__init__.py
+++ src/humainkind/__init__.py
@@ -33,3 +33,17 @@ def get_resource_of_ai(state: Float[ArrayLike, "*dim 2"]) -> Float[jax.Array, "*
     resource = get_resource(state)
     share_of_ai = get_share_of_ai(state)
     return share_of_ai * resource
+
+
+def get_efficacy_of_humankind(
+    state: Float[ArrayLike, "*dim 2"], *, slope: float = 10.0, intercept: float = 2.3
+) -> Float[jax.Array, "*dim"]:
+    resource_of_humankind = get_resource_of_humankind(state)
+    return slope * resource_of_humankind + intercept
+
+
+def get_efficacy_of_ai(
+    state: Float[ArrayLike, "*dim 2"], *, initial_efficacy=1.0, exponent=1.1
+) -> Float[jax.Array, "*dim"]:
+    resource_of_ai = get_resource_of_ai(state)
+    return initial_efficacy * resource_of_ai**exponent
