36441bfaaf2000d898b6d2d9ca5f7ca23f989800
Simon Altrogge <8720147+simonaltrogge@users.noreply.github.com>
2026-08-11 15:56:02 +0200

    ✨ Add state of dynamical system

--- src/humainkind/__init__.py
+++ src/humainkind/__init__.py
@@ -1,2 +1,35 @@
-def hello() -> str:
-    return "Hello from humainkind!"
+import jax
+import jax.numpy as jnp
+from jax.typing import ArrayLike
+from jaxtyping import Float
+
+
+def get_resource(state: Float[ArrayLike, " 2 *dim"]) -> Float[jax.Array, "*dim"]:
+    resource, _ = jnp.asarray(state)
+    return resource
+
+
+def get_share_of_humankind(
+    state: Float[ArrayLike, " 2 *dim"],
+) -> Float[jax.Array, "*dim"]:
+    _, share_of_humankind = jnp.asarray(state)
+    return share_of_humankind
+
+
+def get_share_of_ai(state: Float[ArrayLike, " 2 *dim"]) -> Float[jax.Array, "*dim"]:
+    share_of_humankind = get_share_of_humankind(state)
+    return 1 - share_of_humankind
+
+
+def get_resource_of_humankind(
+    state: Float[ArrayLike, " 2 *dim"],
+) -> Float[jax.Array, "*dim"]:
+    resource = get_resource(state)
+    share_of_humankind = get_share_of_humankind(state)
+    return share_of_humankind * resource
+
+
+def get_resource_of_ai(state: Float[ArrayLike, " 2 *dim"]) -> Float[jax.Array, "*dim"]:
+    resource = get_resource(state)
+    share_of_ai = get_share_of_ai(state)
+    return share_of_ai * resource
