5b844d5fe25016d94545eff4871e0a6a435e1104
Simon Altrogge <8720147+simonaltrogge@users.noreply.github.com>
2026-08-11 18:35:57 +0200

    🏗️ Transpose `state` shape

--- src/humainkind/__init__.py
+++ src/humainkind/__init__.py
@@ -4,32 +4,32 @@ from jax.typing import ArrayLike
 from jaxtyping import Float
 
 
-def get_resource(state: Float[ArrayLike, " 2 *dim"]) -> Float[jax.Array, "*dim"]:
-    resource, _ = jnp.asarray(state)
+def get_resource(state: Float[ArrayLike, "*dim 2"]) -> Float[jax.Array, "*dim"]:
+    resource = jnp.asarray(state)[..., 0]
     return resource
 
 
 def get_share_of_humankind(
-    state: Float[ArrayLike, " 2 *dim"],
+    state: Float[ArrayLike, "*dim 2"],
 ) -> Float[jax.Array, "*dim"]:
-    _, share_of_humankind = jnp.asarray(state)
+    share_of_humankind = jnp.asarray(state)[..., 1]
     return share_of_humankind
 
 
-def get_share_of_ai(state: Float[ArrayLike, " 2 *dim"]) -> Float[jax.Array, "*dim"]:
+def get_share_of_ai(state: Float[ArrayLike, "*dim 2"]) -> Float[jax.Array, "*dim"]:
     share_of_humankind = get_share_of_humankind(state)
     return 1 - share_of_humankind
 
 
 def get_resource_of_humankind(
-    state: Float[ArrayLike, " 2 *dim"],
+    state: Float[ArrayLike, "*dim 2"],
 ) -> Float[jax.Array, "*dim"]:
     resource = get_resource(state)
     share_of_humankind = get_share_of_humankind(state)
     return share_of_humankind * resource
 
 
-def get_resource_of_ai(state: Float[ArrayLike, " 2 *dim"]) -> Float[jax.Array, "*dim"]:
+def get_resource_of_ai(state: Float[ArrayLike, "*dim 2"]) -> Float[jax.Array, "*dim"]:
     resource = get_resource(state)
     share_of_ai = get_share_of_ai(state)
     return share_of_ai * resource
