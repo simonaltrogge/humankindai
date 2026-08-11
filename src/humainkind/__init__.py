4cf3c2b80c329bd267d0c8d581a239c7b291e7a1
Simon Altrogge <8720147+simonaltrogge@users.noreply.github.com>
2026-08-11 19:56:02 +0200

    ⚡️ Convert `ArrayLike` to `Array` immediately

--- src/humainkind/__init__.py
+++ src/humainkind/__init__.py
@@ -5,27 +5,32 @@ from jaxtyping import Float
 
 
 def get_resource(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
-    resource, _ = jnp.asarray(state)
+    state = jnp.asarray(state)
+    resource, _ = state
     return resource
 
 
 def get_share_of_humankind(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
-    _, share_of_humankind = jnp.asarray(state)
+    state = jnp.asarray(state)
+    _, share_of_humankind = state
     return share_of_humankind
 
 
 def get_share_of_ai(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
+    state = jnp.asarray(state)
     share_of_humankind = get_share_of_humankind(state)
     return 1 - share_of_humankind
 
 
 def get_resource_of_humankind(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
+    state = jnp.asarray(state)
     resource = get_resource(state)
     share_of_humankind = get_share_of_humankind(state)
     return share_of_humankind * resource
 
 
 def get_resource_of_ai(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
+    state = jnp.asarray(state)
     resource = get_resource(state)
     share_of_ai = get_share_of_ai(state)
     return share_of_ai * resource
@@ -34,6 +39,7 @@ def get_resource_of_ai(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
 def get_efficacy_of_humankind(
     state: Float[ArrayLike, " 2"], *, slope: float = 10.0, intercept: float = 2.3
 ) -> Float[jax.Array, ""]:
+    state = jnp.asarray(state)
     resource_of_humankind = get_resource_of_humankind(state)
     return slope * resource_of_humankind + intercept
 
@@ -41,6 +47,7 @@ def get_efficacy_of_humankind(
 def get_efficacy_of_ai(
     state: Float[ArrayLike, " 2"], *, initial_efficacy=1.0, exponent=1.1
 ) -> Float[jax.Array, ""]:
+    state = jnp.asarray(state)
     resource_of_ai = get_resource_of_ai(state)
     return initial_efficacy * resource_of_ai**exponent
 
@@ -53,6 +60,7 @@ def get_production_cost(
     learning_rate=0.25,
 ) -> Float[jax.Array, ""]:
     """Learning curve."""
+    state = jnp.asarray(state)
     resource = get_resource(state)
     progress_ratio = 1 - learning_rate
     return initial_cost * (resource / initial_resource) ** jnp.log2(progress_ratio)
