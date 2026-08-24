eadbffd08a8948f5b776913dd115073e9206776f
Simon Altrogge <8720147+simonaltrogge@users.noreply.github.com>
2026-08-24 11:39:21 +0200

    🏷️ Increase type-safety
    Stop accepting array-like inputs. Provide a function to create a state
    object of appropriate type and have all functions expect this type as
    input.

--- src/humainkind/__init__.py
+++ src/humainkind/__init__.py
@@ -1,80 +1,73 @@
 import jax
 import jax.numpy as jnp
-from jax.typing import ArrayLike
 from jaxtyping import Float
 
 
-def get_resource(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
-    state = jnp.asarray(state)
+def create_state(resource: float, share_of_humankind: float) -> Float[jax.Array, " 2"]:
+    return jnp.array([resource, share_of_humankind])
+
+
+def get_resource(state: Float[jax.Array, " 2"]) -> Float[jax.Array, ""]:
     resource, _ = state
     return resource
 
 
-def get_share_of_humankind(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
-    state = jnp.asarray(state)
+def get_share_of_humankind(state: Float[jax.Array, " 2"]) -> Float[jax.Array, ""]:
     _, share_of_humankind = state
     return share_of_humankind
 
 
-def get_share_of_ai(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
-    state = jnp.asarray(state)
+def get_share_of_ai(state: Float[jax.Array, " 2"]) -> Float[jax.Array, ""]:
     share_of_humankind = get_share_of_humankind(state)
     return 1 - share_of_humankind
 
 
-def get_resource_of_humankind(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
-    state = jnp.asarray(state)
+def get_resource_of_humankind(state: Float[jax.Array, " 2"]) -> Float[jax.Array, ""]:
     resource = get_resource(state)
     share_of_humankind = get_share_of_humankind(state)
     return share_of_humankind * resource
 
 
-def get_resource_of_ai(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, ""]:
-    state = jnp.asarray(state)
+def get_resource_of_ai(state: Float[jax.Array, " 2"]) -> Float[jax.Array, ""]:
     resource = get_resource(state)
     share_of_ai = get_share_of_ai(state)
     return share_of_ai * resource
 
 
 def get_efficacy_of_humankind(
-    state: Float[ArrayLike, " 2"],
+    state: Float[jax.Array, " 2"],
     *,
     conversion_factor: float = 0.0002329,
     slope: float = 10.01,
     intercept: float = 2.31,
 ) -> Float[jax.Array, ""]:
-    state = jnp.asarray(state)
     resource_of_humankind = get_resource_of_humankind(state)
     return conversion_factor * (slope * resource_of_humankind + intercept)
 
 
 def get_efficacy_of_ai(
-    state: Float[ArrayLike, " 2"], *, initial_efficacy=0.124575, exponent=1.1
+    state: Float[jax.Array, " 2"], *, initial_efficacy=0.124575, exponent=1.1
 ) -> Float[jax.Array, ""]:
-    state = jnp.asarray(state)
     resource_of_ai = get_resource_of_ai(state)
     return initial_efficacy * resource_of_ai**exponent
 
 
 def get_production_cost(
-    state: Float[ArrayLike, " 2"],
+    state: Float[jax.Array, " 2"],
     *,
     initial_cost=1.0,
     initial_resource=9.19963,
     learning_rate=0.25,
 ) -> Float[jax.Array, ""]:
     """Learning curve."""
-    state = jnp.asarray(state)
     resource = get_resource(state)
     progress_ratio = 1 - learning_rate
     return initial_cost * (resource / initial_resource) ** jnp.log2(progress_ratio)
 
 
 def get_greedy_dynamics_of_humankind(
-    state: Float[ArrayLike, " 2"], *, redistribution_cost: float = 50.0
+    state: Float[jax.Array, " 2"], *, redistribution_cost: float = 50.0
 ) -> Float[jax.Array, " 2"]:
-    state = jnp.asarray(state)
-
     production_cost = get_production_cost(state)
     cost_vector = jnp.array([production_cost, redistribution_cost])
 
@@ -93,10 +86,8 @@ def get_greedy_dynamics_of_humankind(
 
 
 def get_greedy_dynamics_of_ai(
-    state: Float[ArrayLike, " 2"], *, redistribution_cost: float = 250.0
+    state: Float[jax.Array, " 2"], *, redistribution_cost: float = 250.0
 ) -> Float[jax.Array, " 2"]:
-    state = jnp.asarray(state)
-
     production_cost = get_production_cost(state)
     cost_vector = jnp.array([production_cost, redistribution_cost])
 
@@ -112,7 +103,5 @@ def get_greedy_dynamics_of_ai(
     return efficacy_of_ai * (normalized_cost_weighted_grad_resource_of_ai / cost_vector)
 
 
-def get_greedy_dynamics(state: Float[ArrayLike, " 2"]) -> Float[jax.Array, " 2"]:
-    state = jnp.asarray(state)
-
+def get_greedy_dynamics(state: Float[jax.Array, " 2"]) -> Float[jax.Array, " 2"]:
     return get_greedy_dynamics_of_humankind(state) + get_greedy_dynamics_of_ai(state)
