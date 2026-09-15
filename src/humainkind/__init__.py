70e38b59b7df057cda3f5c74faad43d32e3de08a
Simon Altrogge <8720147+simonaltrogge@users.noreply.github.com>
2026-09-15 10:49:58 +0200

    🚸 Tweak relative perturbation magnitudes
    Choose relative perturbations two orders of magnitude larger than
    relative tolerance.

--- src/humainkind/__init__.py
+++ src/humainkind/__init__.py
@@ -423,9 +423,9 @@ def predict_gradients_of_resources_of_humankind_and_ai(
         (share_of_humankind == 0.0) | (share_of_ai == 0.0), 0.0, planning_horizon
     )
 
-    resource_perturbation = resource * 1e-6
+    resource_perturbation = resource * 1e-8
     share_of_humankind_perturbation = jnp.max(
-        jnp.array([share_of_humankind * 1e-6, 1e-9])
+        jnp.array([share_of_humankind * 1e-8, 1e-9])
     )  # Prevent perturbation becoming zero when `share_of_humankind` is zero.
 
     resource_upwards_perturbation = resource_perturbation
